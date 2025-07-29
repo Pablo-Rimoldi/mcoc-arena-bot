8172ad4ad6f7164ad14b0adb52884daedf66f612
PaRi29 <87933597+PaRi29@users.noreply.github.com>
2025-07-29 17:31:28 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,23 +1,21 @@
 from typing import Tuple
 import os
-
+import numpy as np
 
 class Config:
     ASSETS_DIR: str = os.path.join(
         os.path.dirname(__file__), '../assets/data/raw')
     FPS: int = 30
-    MCOC_BORDER_COLOR_BGR: Tuple[int, int, int] = (
-        13, 1, 208)  # rgba(208,1,13,255)
-    COLOR_TOLERANCE: int = 15
-    MIN_GAME_AREA: int = 240000
-    GAME_ASPECT_RATIO_MIN: float = 1.2
-    GAME_ASPECT_RATIO_MAX: float = 2.5
-    MIN_GAME_WIDTH: int = 600
-    MIN_GAME_HEIGHT: int = 400
+    TARGET_COLOR_BGR = np.array([12, 0, 208])    
+    COLOR_TOLERANCE: int = 20
+    MIN_WIDTH: int = 600
+    MIN_HEIGHT: int = 400
+    TOP_BAR_CROP_PERCENTAGE = 0.085
+    SIDE_MARGIN = 3
+    BOTTOM_MARGIN = 3
 
     # Template matching config
     TEMPLATE_IMAGE_PATH: str = r"assets\config\pause.png"
     TEMPLATE_MATCH_THRESHOLD: float = 0.85
-
 
 config = Config()
